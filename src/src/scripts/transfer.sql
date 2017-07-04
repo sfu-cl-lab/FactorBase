a98ae96f4503879092b0e1a1a43b772a96a4d61c
oschulte <oschulte@cs.sfu.ca>
2017-07-04 07:57:04 -0700

    Update transfer.sql
    copying only a subset of functors and population variables from setup metadata database

--- src/src/scripts/transfer.sql
+++ src/src/scripts/transfer.sql
@@ -8,13 +8,25 @@ USE @database@_BN;
 SET storage_engine=INNODB;
 
 
-/*copy tables from setup database */
+/*copy tables from setup metadata database into learning database
+/*copy subset as specified in setup  rather than full functor set */
 
+create table Fnodes as select * from @database@_setup.FunctorSet; /*create subset of functor nodes */
+create table 1Nodes as select * from @database@_setup.1Nodes N, Fnodes F where N.1nid = F.fid;
+create table 2Nodes as select * from @database@_setup.2Nodes N, Fnodes F where N.2nid = F.Fid;
+create table RNodes as select * from @database@_setup.RNodes N, Fnodes F where N.rnid = F.Fid;
+create table FNodes_pvars select * from @database@_setup.FNodes_pvars FP where FP.fid in (select fid from Fnodes);
+create table PVariables as select * from @database@_setup.PVariables P where P.pvid in (select pvid from FNodes_pvars);
+/*copy only Pvariables that appear in functor subset */
+create table Groundings like @database@_setup.Groundings; 
+insert into Groundings select * from @database@_setup.Groundings;
+/* the like construct should preserve foreign key pointers to Pvariables from the setup database. 
+/* Want to force groundings to target Pvariables that appear in the functor subset.*/
 
-create table 1Nodes as select * from @database@_setup.1Nodes;
-create table 2Nodes as select * from @database@_setup.2Nodes;
-create table RNodes as select * from @database@_setup.RNodes;
-create table PVariables as select * from @database@_setup.PVariables;
+/*the remaining tables are copied as they are */
+/*they contain information about all functors and population variables, but it should work just as look ups for the restricted subsets.
+/*otherwise need to make restricted versions of these as well */
+
 create table EntityTables as select * from @database@_setup.EntityTables;
 create table AttributeColumns as select * from @database@_setup.AttributeColumns;
 create table TernaryRelations as select * from @database@_setup.TernaryRelations;
@@ -24,11 +36,9 @@ create table ForeignKeyColumns as select * from  @database@_setup.ForeignKeyColu
 create table ForeignKeys_pvars as select * from  @database@_setup.ForeignKeys_pvars;
 create table InputColumns as select * from  @database@_setup.InputColumns;
 create table Attribute_Value as select * from  @database@_setup.Attribute_Value;
-/*
-create table Groundings like @database@_setup.Groundings; 
-insert into Groundings select * from @database@_setup.Groundings;
-*/
 
+
+/* these tables are actually created later in metadata_3 */
 /*
 create table Path_BN_nodes as select * from  @database@_setup.Path_BN_nodes;
 create table lattice_membership as select * from  @database@_setup.lattice_membership;
