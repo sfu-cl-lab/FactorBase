06264702384f3a60a381c7abf77604322429fe60
Vidhi Jain <vidhiJain@users.noreply.github.com>
2017-07-13 22:09:04 -0700

    Update BayesBaseCT_SortMerge.java

--- src/BayesBaseCT_SortMerge.java
+++ src/BayesBaseCT_SortMerge.java
@@ -102,22 +102,6 @@ public class BayesBaseCT_SortMerge {
         // // building CT tables for Rchain
         con_CT = connectDB(databaseName_CT);
 
-
-        System.out.println(" ##### lattice is ready for use* ");
-
-        //build _BN part2: from metadata_2.sql
-        // empty query error,fixed by removing one duplicated semicolon. Oct 30, 2013
-        //ToDo: No support for executing LinkCorrelation=0;
-        if (cont.equals("1")) {
-            bzsr.runScript("scripts/metadata_2_cont.sql");
-        } else if (linkCorrelation.equals("1")) { //LinkCorrelations
-            bzsr.runScript("scripts/metadata_2.sql");
-        } else {
-            bzsr.runScript("scripts/metadata_2.sql");
-            // modified on Feb. 3rd, 2015, zqian, to include rnode as columns
-        //			bzsr.runScript("scripts/metadata_2_nolink.sql");
-        }
-
         // building CT tables for Rchain
         CTGenerator();
         disconnectDB();
