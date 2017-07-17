3c186dfcffc6887ce74b1b94d80b122557c13b1a
oschulte <oschulte@cs.sfu.ca>
2017-07-17 14:05:37 -0700

    Update query_list.sql
    adding clauses for expansions to select and where lists

--- src/scripts/query_list.sql
+++ src/scripts/query_list.sql
@@ -50,6 +50,8 @@ WHERE
 /* use entity tables for main variables only (index = 0). 
 Other entity tables have empty Bayes nets by the main functor constraint. */
 
+/* the pvariable select list contains the union of select list for each 1node associated with the pvariable */
+
 CREATE TABLE PVariables_Select_List AS 
 SELECT 
     pvid, CONCAT('count(*)',' as "MULT"') AS Entries
@@ -67,7 +69,6 @@ WHERE
     UNION
  /*for each pvariable in expansion, find the primary column and add it to the select list */
  /* don't use this for continuous, but do use it for the no_link case */
- 
  SELECT E.pvid, CONCAT(E.pvid,'.',REFERENCED_COLUMN_NAME) AS Entries FROM
  RNodes_pvars RP, Expansions E where E.pvid = RP.pvid;
  
@@ -90,7 +91,13 @@ SELECT pvid,
 Now we make data join tables for each relationship functor node.
 ***********/
 
-/* for each relationship functor, for each population variable, find the column name that is a foreign key pointer to the Entity table for the population, and find the name the referenced column in the Entity table. */
+
+/* the from list for the relationship functor contains: 
+1) the relationship table 
+2) the union of the from lists of the population variables associated with the relationship node
+/* for each relationship functor, for each population variable, 
+find the column name that is a foreign key pointer to the Entity table for the population, 
+and find the name the referenced column in the Entity table. */
 
 
 
@@ -115,6 +122,9 @@ from
 We need to replace the apostrophes in rnid to make the rnid a valid name for the temporary table 
 **/
 
+/** The Where List for each rnode contains the join condition that links the foreign key pointers in the relationship table to
+the ids in the population entity tables. **/
+
 CREATE TABLE RNodes_Where_List AS SELECT rnid,
     CONCAT(rnid,
             '.',
@@ -137,6 +147,8 @@ FROM
     
     /* the table RNodes_pvars is such useful | added by zqian*/
 
+/** now link each rnode id to the associated population variables **/
+
 CREATE TABLE RNodes_1Nodes AS SELECT rnid, TABLE_NAME, 1nid, COLUMN_NAME, pvid1 AS pvid FROM
     RNodes,
     1Nodes
@@ -150,12 +162,20 @@ FROM
 WHERE
     1Nodes.pvid = RNodes.pvid2;
 
+/** now link each rnode 2node, i.e. each attribute of the relationship to the associated 2nodes **/
+
 Create TABLE RNodes_2Nodes as 
 select RNodes.rnid, 2Nodes.2nid from 2Nodes, RNodes where 2Nodes.TABLE_NAME = RNodes.TABLE_NAME; 
 
 
-/** Each unary functor, and each binary functor, becomes an attribute to be retrieved in the select list.
-Make a table to record this. **/
+/** The select list for an rnode contains
+1) the rnid itself
+2) the union of the selet lists for each population variable (currently implemented as 1nid)
+3) the 2nodes of the relationship variable 
+4) 
+
+***/
+
 CREATE TABLE RNodes_Select_List AS 
 select 
     rnid, concat('count(*)',' as "MULT"') AS Entries
@@ -181,6 +201,11 @@ select
     rnid, rnid AS Entries
 from
     RNodes 
+    UNION DISTINCT
+    /*for each associated to rnode pvariable in expansion , find the primary column and add it to the group by list */
+ /* don't use this for continuous, but do use it for the no_link case */
+    SELECT distinct rnid, PV.Entries
+FROM unielwin_BN.RNodes_pvars RP, PVariables_Select_List PV where RP.pvid = PV.pvid;
 ;
 
 CREATE TABLE RNodes_GroupBy_List AS SELECT DISTINCT rnid, 1nid AS Entries FROM
@@ -196,4 +221,7 @@ UNION distinct
 select 
     rnid, rnid
 from
-    RNodes;
+    RNodes
+    UNION DISTINCT
+    SELECT distinct rnid, PV.Entries
+FROM unielwin_BN.RNodes_pvars RP, PVariables_GroupBy_List PV where RP.pvid = PV.pvid;
