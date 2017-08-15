7932204112e20e2f1701619b507f732e2de5a6e4
oschulte <oschulte@cs.sfu.ca>
2017-08-15 16:30:44 -0700

    Update transfer.sql
    transferring files from setup to BN. Need to restrict PVariables to FunctorSet

--- src/scripts/transfer.sql
+++ src/scripts/transfer.sql
@@ -11,7 +11,7 @@ USE @database@_BN;
 SET storage_engine=INNODB;
 
 
-/*copy tables from setup database */
+/*copy tables from setup database. restricted to functor set */
 CREATE TABLE FunctorSet AS SELECT * FROM
     unielwin_setup.FunctorSet;
 
@@ -45,19 +45,15 @@ WHERE
     N.orig_rnid = F.Fid;
 
 
-create table 1Nodes as select * from @database@_setup.1Nodes;
-create table 2Nodes as select * from @database@_setup.2Nodes;
-create table RNodes as select * from @database@_setup.RNodes;
 create table PVariables as select * from @database@_setup.PVariables;
 create table EntityTables as select * from @database@_setup.EntityTables;
 create table AttributeColumns as select * from @database@_setup.AttributeColumns;
-create table TernaryRelations as select * from @database@_setup.TernaryRelations;
+/* create table TernaryRelations as select * from @database@_setup.TernaryRelations; */
 create table RelationTables as select * from @database@_setup.RelationTables;
-create table NoPKeys as select * from  @database@_setup.NoPKeys;
+/* create table NoPKeys as select * from  @database@_setup.NoPKeys; */
 create table ForeignKeyColumns as select * from  @database@_setup.ForeignKeyColumns;
 create table ForeignKeys_pvars as select * from  @database@_setup.ForeignKeys_pvars;
 create table InputColumns as select * from  @database@_setup.InputColumns;
-create table Attribute_Value as select * from  @database@_setup.Attribute_Value;
 create table Expansions as select * from @database@_setup.Expansions;
 /*
 create table Groundings like @database@_setup.Groundings; 
