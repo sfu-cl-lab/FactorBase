adedfbe348b6e9a1ba0f46fae0e73d27192bd6f3
rmar3a <rkm3@sfu.ca>
2018-03-22 17:03:13 -0700

    Merged methods in BayesNet_Learning_main_dc.java into BayesNet_Learning_main.java.
    - BayesNet_Learning_main_dc.java contained methods that used a global
      cache; however, it is not necessary to have this code in a separate
      file as the parameters are different than the ones found in the
      original BayesNet_Learning_main.java file.  Hence, the merging of
      these files should be safe as the methods become overloaded methods.
    - Removed BayesNet_Learning_main_dc.java since its unique code is now
      in BayesNet_Learning_main.java.

--- src/ca/sfu/jbn/BayesNet_Learning_main.java
+++ src/ca/sfu/jbn/BayesNet_Learning_main.java
@@ -30,6 +30,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class BayesNet_Learning_main {
 
@@ -56,7 +58,12 @@ public class BayesNet_Learning_main {
 	{
 		tetradLearner(srcfile, null, null, destfile);
 	}
-	
+
+    public static void tetradLearner(String srcfile, String destfile, Map<Node, Map<Set<Node>, Double>> globalScoreHash) throws Exception
+    {
+        tetradLearner(srcfile, null, null, destfile, globalScoreHash);
+    }
+
 	public static void tetradLearner(String srcfile, String required, String forbidden, String destfile) throws Exception
 	{
 		/* initialization */
@@ -218,6 +225,176 @@ public class BayesNet_Learning_main {
 		*/
 	}
 
+    public static void tetradLearner(String srcfile, String required, String forbidden, String destfile, Map<Node, Map<Set<Node>, Double>> globalScoreHash) throws Exception
+    {
+        /* initialization */
+        //TetradLogger.getInstance().addOutputStream(System.out);
+        //RectangularDataSet dataset = null;
+        //RectangularDataSet is replaced by DataSet in new tetrad.  Aug 21 Yan
+        DataSet dataset = null;
+
+        File src = new File(srcfile);
+
+        //DataParser parser = new DataParser();
+        //DataParser is replaced by DataReader in new tetrad.  Aug 21 Yan
+        DataReader parser = new DataReader();
+        parser.setDelimiter(DelimiterType.TAB);
+        dataset = parser.parseTabular(src);
+
+        //before_dataset = parser.parseTabular(src);
+        //TetradLogger.getInstance().removeOutputStream(System.out);
+        //System.out.print(before_dataset);
+        //System.out.print("\n");
+        //System.out.print("isMulipliersCollapsed: " +before_dataset.isMulipliersCollapsed()+" \n");
+        System.out.print("isMulipliersCollapsed: " +dataset.isMulipliersCollapsed()+" \n");
+        //CaseExpander CE= new CaseExpander(); //@zqian //expanding the dataset
+        //dataset = CE.filter(before_dataset);
+        //System.out.print(dataset);
+
+        //GesSearch gesSearch = new GesSearch(dataset);
+        Ges3 gesSearch = new Ges3(dataset);
+        Knowledge knowledge = new Knowledge();
+
+        /* load required knowledge */
+        if (required != null)
+        {
+            Builder xmlParser = new Builder();
+            //Document doc = xmlParser.build(required);
+            Document doc = xmlParser.build(new File(required));
+            Element root = doc.getRootElement();
+            root = root.getFirstChildElement("NETWORK");
+            Elements requiredEdges = root.getChildElements("DEFINITION");
+
+            for (int i=0; i<requiredEdges.size(); i++)
+            {
+                Element node = requiredEdges.get(i);
+                Element child = node.getFirstChildElement("FOR");
+                Elements parents = node.getChildElements("GIVEN");
+                for (int j=0; j<parents.size(); j++)
+                {
+                    Element parent = parents.get(j);
+                    String childStr = child.getValue(), parentStr = parent.getValue();
+                    //System.out.println("Test, Required: " + parentStr + " " + childStr);
+                    knowledge.setEdgeRequired(parentStr, childStr, true);
+                }
+            }
+        }
+
+        /* load forbidden knowledge */
+        if (forbidden != null)
+        {
+            Builder xmlParser = new Builder();
+            //Document doc = xmlParser.build(forbidden);
+            Document doc = xmlParser.build(new File(forbidden));
+            Element root = doc.getRootElement();
+            root = root.getFirstChildElement("NETWORK");
+            Elements forbiddenEdges = root.getChildElements("DEFINITION");
+
+            for (int i=0; i<forbiddenEdges.size(); i++)
+            {
+                Element node = forbiddenEdges.get(i);
+                Element child = node.getFirstChildElement("FOR");
+                Elements parents = node.getChildElements("GIVEN");
+                for (int j=0; j<parents.size(); j++)
+                {
+                    Element parent = parents.get(j);
+                    String childStr = child.getValue(), parentStr = parent.getValue();
+                    //System.out.println("Test, Forbidden: " + parentStr + " " + childStr);
+                    knowledge.setEdgeForbidden(parentStr, childStr, true);
+                }
+            }
+        }
+
+        System.out.print(knowledge);
+        System.out.println("knowledge is DONE~~");
+        /* set GES search parameters */
+        gesSearch.setKnowledge(knowledge);
+        gesSearch.setStructurePrior(1.0000);
+        gesSearch.setSamplePrior(10.0000);
+
+        /* learn a dag from data */
+        /** Diljot,Chris : using the globalScoreHash from BayesBase.java in the function.
+         * the search function gets our globalScoreHash. Used by ges3 to keep scores and
+         * since it is not local to ges3, it's not cleared between calls anymore.
+         * */
+        Graph graph = gesSearch.search(globalScoreHash);
+        Pattern pattern = new Pattern(graph);
+
+        //PatternToDagSearch is replaced by PatternToDag in new tetrad. Aug 21 Yan
+        //PatternToDagSearch p2d = new PatternToDagSearch(pattern);
+        PatternToDag p2d = new PatternToDag(pattern);
+        Dag dag = p2d.patternToDagMeek();
+        System.out.println("Final DAG Starts");
+        System.out.println(dag);
+        System.out.println("DAG is DONE~~~");
+
+        ///////////////////////////////////////////
+        //April 16th @zqian here the score can not be used for comparison since it's based on the much smaller join
+    /*  BayesProperties scorer = new BayesProperties(dataset,dag);
+		System.out.println("%%%% Tuples of Data = " + dataset.getNumRows());
+		System.out.println("%%%% BIC score = "+scorer.getBic());
+		System.out.println("%%%% AIC score = "+scorer.getAic());
+		System.out.println("%%%% loglikelihood = " + scorer.getloglikelihood());
+		System.out.println("%%%% P-value = " + scorer.getPValue());
+        System.out.println("%%%% Chisq = " + scorer.getPValueChisq());
+        System.out.println("%%%% Dof = " + scorer.getPValueDf());
+        */
+        ////////////////////// computing these score is very time consuming
+
+        /* output dag into Bayes Interchange format */
+        FileWriter fstream = new FileWriter(destfile);
+        BufferedWriter out = new BufferedWriter(fstream);
+        out.write(BIFHeader.header);
+        out.write("<BIF VERSION=\"0.3\">\n");
+        out.write("<NETWORK>\n");
+        out.write("<NAME>BayesNet</NAME>\n");
+
+        int col = dataset.getNumColumns(), row = dataset.getNumRows();
+        for (int i=0; i<col; i++)
+        {
+            out.write("<VARIABLE TYPE=\"nature\">\n");
+            out.write("\t<NAME>" + "`"+dataset.getVariable(i).getName() +"`"+ "</NAME>\n"); //@zqian adding apostrophes to the name of bayes nodes
+            HashSet<Object> domain = new HashSet<Object>();
+            for (int j=0; j<row; j++)
+                domain.add(dataset.getObject(j, i));
+            for (Object o : domain)
+            {
+                out.write("\t<OUTCOME>" + o + "</OUTCOME>\n");
+            }
+            out.write("</VARIABLE>\n");
+        }
+
+        List<Node> nodes = dag.getNodes();
+        int nodesNum = nodes.size();
+        for (int i=0; i<nodesNum; i++)
+        {
+            Node current = nodes.get(i);
+            List<Node> parents = dag.getParents(current);
+            int parentsNum = parents.size();
+            out.write("<DEFINITION>\n");
+            out.write("\t<FOR>" +"`"+ current +"`"+ "</FOR>\n"); //@zqian
+            for (int j=0; j<parentsNum; j++)
+                out.write("\t<GIVEN>" +"`"+ parents.get(j) +"`"+ "</GIVEN>\n");//@zqian
+            out.write("</DEFINITION>\n");
+        }
+
+        out.write("</NETWORK>\n");
+        out.write("</BIF>\n");
+        out.close();
+
+        /*//@zqian : redirectinig the dag to file: dag.txt.
+        // only need the final dag
+        File file = new File("dag.txt");
+        PrintStream console = System.out;
+        FileOutputStream fos = new FileOutputStream(file);
+        PrintStream ps = new PrintStream(fos);
+        System.setOut(ps);
+        System.out.println(dag);
+        System.setOut(console);
+        */
+    }
+
+    // TODO: Figure out if tetradLearner_BES can be deleted since it doesn't appear to be used anywhere.
 	/*pruning phase, zqian @ Oct 23 2013*/
 	public static void tetradLearner_BES(String srcfile, String required, String destfile) throws Exception
 	{
@@ -337,7 +514,6 @@ public class BayesNet_Learning_main {
 		*/
 	}
 
-	
 	public static void jbnMain(String[] args) throws Exception
 	{
 		if (args.length < 1)
@@ -356,6 +532,23 @@ public class BayesNet_Learning_main {
 		}
 	}
 
+    public static void jbnMain(String[] args, Map<Node, Map<Set<Node>, Double>> globalScoreHash) throws Exception
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Usage: bayesLearner <csv>");
+            System.out.println("Usage: bayerLearner <csv> <required knowledge> <forbidden knowledge>");
+            System.exit(0);
+        }
+
+        if (args.length < 3)
+        {
+            BayesNet_Learning_main.tetradLearner(args[0], null, null, "bif.xml",globalScoreHash);
+        } else
+        {
+            BayesNet_Learning_main.tetradLearner(args[0], args[1], args[2], "bif.xml",globalScoreHash);
+        }
+    }
 }
 
 class BIFHeader {
