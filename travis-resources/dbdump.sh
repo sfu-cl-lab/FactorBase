b6ca2d6a0aa3292b1fcdc15103de134f6dca3197
rmar3a <rkm3@sfu.ca>
2018-09-18 02:22:58 -0700

    Updated the Travis build to do additional validation.
    - Since we don't have very many unit tests this validation will help
      us detect any unwanted changes.
    - The Travis build now checks that the BIF file generated by
      FactorBase for unielwin looks correct.
    - Created a Bash script to extract the data from the FactorBase MySQL
      tables.
    - The Travis build also checks that the tables produced by FactorBase
      for unielwin look correct by using the Bash script.
    
    Note: The contents of the expected-output directory should be updated
          when an intentional change is made; otherwise, any differences
          found in the diff will let us know of unwanted changes.

---
+++ travis-resources/dbdump.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -o pipefail
+
+CLEAR='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+output="mysql-extraction.txt"
+defaultName="unielwin"
+mysqlCommand="mysql -u root -B"
+
+while getopts "n:p:" argument
+do
+  case $argument in
+    n) name=$OPTARG
+       ;;
+    p) password=$OPTARG
+  esac
+done
+
+if [[ -z $name ]]
+then
+  name=$defaultName
+fi
+
+if [[ -n $password ]]
+then
+  mysqlCommand="$mysqlCommand -p$password -e"
+else
+  mysqlCommand="$mysqlCommand -e"
+fi
+
+>$output
+
+any_failure=0
+
+for database in "${name}_BN" "${name}_CT" "${name}_convert_CT" "${name}_setup"
+do
+  extractionFailed=0
+  echo "Now extracting tables in database: $database..."
+
+  tables=$($mysqlCommand "use $database; show tables;" | tail -n +2)
+
+  if [[ $? -ne 0 ]]
+  then
+    extractionFailed=1
+    any_failure=1
+  fi
+
+  for table in $tables
+  do
+    echo "  Extracting table: $table"
+    echo "Table: $table" >> $output
+    $mysqlCommand "use $database; select * from \`$table\`;" | sort >> $output
+    if [[ $? -ne 0 ]]
+    then
+      extractionFailed=1
+      any_failure=1
+    fi
+  done
+
+  if [[ $extractionFailed -eq 0 ]]
+  then
+    echo -e "\n  ${GREEN}Extraction Complete!${CLEAR}\n"
+  else
+    echo -e "\n  ${RED}Extraction Failed!${CLEAR}\n"
+  fi
+done
+
+if [[ $any_failure -eq 1 ]]
+then
+  echo -e "${RED}Failed to extract data!${CLEAR}"
+  exit 1
+else
+    echo -e "  ${GREEN}ALL GOOD!${CLEAR}\n"
+    echo -e "  The extracted data can be found in $output.\n"
+fi
