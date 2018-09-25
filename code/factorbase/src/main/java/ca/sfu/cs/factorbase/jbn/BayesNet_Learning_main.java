168ff312dd09a04a1eff298667a6ee5f4b5c1bf7
rmar3a <rkm3@sfu.ca>
2018-09-25 16:02:48 -0700

    [deadcode] Removed unnecessary main() methods in BayesNet_Learning_main.java.
    - Removed unused imports.
    - Removed the main() methods in BayesNet_Learning_main.java since the
      code tested by these methods can be tested by putting breakpoints
      while debugging the code in an IDE like Eclipse.  Alternatively,
      a separate file could be created to run the code, but it seems
      like unnecessary bloat to the codebase so deciding to just remove
      the code.
    - Removed the Tetrad Learner method that uses a global cache i.e.
    
      tetradLearner(
          String srcfile,
          String required,
          String forbidden,
          String destfile,
          Map<Node, Map<Set<Node>, Double>> globalScoreHash
      )
    
      since it isn't used by FactorBase except in the main methods that
      are being removed in this commit.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/jbn/BayesNet_Learning_main.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/jbn/BayesNet_Learning_main.java
@@ -20,8 +20,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
 public class BayesNet_Learning_main {
@@ -145,157 +143,6 @@ public class BayesNet_Learning_main {
         out.write("</BIF>\n");
         out.close();
     }
-
-
-    public static void tetradLearner(String srcfile, String required, String forbidden, String destfile, Map<Node, Map<Set<Node>, Double>> globalScoreHash) throws Exception {
-        DataSet dataset = null;
-
-        File src = new File(srcfile);
-
-        DataReader parser = new DataReader();
-        parser.setDelimiter(DelimiterType.TAB);
-        dataset = parser.parseTabular(src);
-
-        System.out.print("isMulipliersCollapsed: " + dataset.isMulipliersCollapsed() + " \n");
-
-        Ges3 gesSearch = new Ges3(dataset);
-        Knowledge knowledge = new Knowledge();
-
-        /* load required knowledge */
-        if (required != null) {
-            Builder xmlParser = new Builder();
-            Document doc = xmlParser.build(new File(required));
-            Element root = doc.getRootElement();
-            root = root.getFirstChildElement("NETWORK");
-            Elements requiredEdges = root.getChildElements("DEFINITION");
-
-            for (int i = 0; i < requiredEdges.size(); i++) {
-                Element node = requiredEdges.get(i);
-                Element child = node.getFirstChildElement("FOR");
-                Elements parents = node.getChildElements("GIVEN");
-                for (int j = 0; j < parents.size(); j++) {
-                    Element parent = parents.get(j);
-                    String childStr = child.getValue();
-                    String parentStr = parent.getValue();
-                    knowledge.setEdgeRequired(parentStr, childStr, true);
-                }
-            }
-        }
-
-        /* load forbidden knowledge */
-        if (forbidden != null) {
-            Builder xmlParser = new Builder();
-            Document doc = xmlParser.build(new File(forbidden));
-            Element root = doc.getRootElement();
-            root = root.getFirstChildElement("NETWORK");
-            Elements forbiddenEdges = root.getChildElements("DEFINITION");
-
-            for (int i = 0; i < forbiddenEdges.size(); i++) {
-                Element node = forbiddenEdges.get(i);
-                Element child = node.getFirstChildElement("FOR");
-                Elements parents = node.getChildElements("GIVEN");
-                for (int j = 0; j < parents.size(); j++) {
-                    Element parent = parents.get(j);
-                    String childStr = child.getValue(), parentStr = parent.getValue();
-                    knowledge.setEdgeForbidden(parentStr, childStr, true);
-                }
-            }
-        }
-
-        System.out.print(knowledge);
-        System.out.println("knowledge is DONE~~");
-        /* set GES search parameters */
-        gesSearch.setKnowledge(knowledge);
-        gesSearch.setStructurePrior(1.0000);
-        gesSearch.setSamplePrior(10.0000);
-
-        /* learn a dag from data */
-        /** Diljot,Chris : using the globalScoreHash from BayesBase.java in the function.
-         * the search function gets our globalScoreHash. Used by ges3 to keep scores and
-         * since it is not local to ges3, it's not cleared between calls anymore.
-         */
-        Graph graph = gesSearch.search(globalScoreHash);
-        Pattern pattern = new Pattern(graph);
-
-        PatternToDag p2d = new PatternToDag(pattern);
-        Dag dag = p2d.patternToDagMeek();
-        System.out.println("Final DAG Starts");
-        System.out.println(dag);
-        System.out.println("DAG is DONE~~~");
-
-        /* output dag into Bayes Interchange format */
-        FileWriter fstream = new FileWriter(destfile);
-        BufferedWriter out = new BufferedWriter(fstream);
-        out.write(BIFHeader.header);
-        out.write("<BIF VERSION=\"0.3\">\n");
-        out.write("<NETWORK>\n");
-        out.write("<NAME>BayesNet</NAME>\n");
-
-        int col = dataset.getNumColumns();
-        int row = dataset.getNumRows();
-        for (int i = 0; i < col; i++) {
-            out.write("<VARIABLE TYPE=\"nature\">\n");
-            out.write("\t<NAME>" + "`" + dataset.getVariable(i).getName() + "`" + "</NAME>\n"); // @zqian adding apostrophes to the name of bayes nodes
-            HashSet<Object> domain = new HashSet<Object>();
-            for (int j = 0; j < row; j++) {
-                domain.add(dataset.getObject(j, i));
-            }
-            for (Object o : domain) {
-                out.write("\t<OUTCOME>" + o + "</OUTCOME>\n");
-            }
-
-            out.write("</VARIABLE>\n");
-        }
-
-        List<Node> nodes = dag.getNodes();
-        int nodesNum = nodes.size();
-        for (int i = 0; i < nodesNum; i++) {
-            Node current = nodes.get(i);
-            List<Node> parents = dag.getParents(current);
-            int parentsNum = parents.size();
-            out.write("<DEFINITION>\n");
-            out.write("\t<FOR>" + "`" + current + "`" + "</FOR>\n"); // @zqian
-            for (int j = 0; j < parentsNum; j++) {
-                out.write("\t<GIVEN>" + "`" + parents.get(j) + "`" + "</GIVEN>\n"); // @zqian
-            }
-
-            out.write("</DEFINITION>\n");
-        }
-
-        out.write("</NETWORK>\n");
-        out.write("</BIF>\n");
-        out.close();
-    }
-
-
-    public static void jbnMain(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.out.println("Usage: bayesLearner <csv>");
-            System.out.println("Usage: bayerLearner <csv> <required knowledge> <forbidden knowledge>");
-            System.exit(0);
-        }
-
-        if (args.length < 3) {
-            BayesNet_Learning_main.tetradLearner(args[0], null, null, "bif.xml");
-        } else {
-            BayesNet_Learning_main.tetradLearner(args[0], args[1], args[2], "bif.xml");
-        }
-    }
-
-
-    public static void jbnMain(String[] args, Map<Node, Map<Set<Node>, Double>> globalScoreHash) throws Exception {
-        if (args.length < 1) {
-            System.out.println("Usage: bayesLearner <csv>");
-            System.out.println("Usage: bayerLearner <csv> <required knowledge> <forbidden knowledge>");
-            System.exit(0);
-        }
-
-        if (args.length < 3) {
-            BayesNet_Learning_main.tetradLearner(args[0], null, null, "bif.xml",globalScoreHash);
-        } else {
-            BayesNet_Learning_main.tetradLearner(args[0], args[1], args[2], "bif.xml",globalScoreHash);
-        }
-    }
 }
 
 
@@ -319,9 +166,4 @@ class BIFHeader {
         "	<!ELEMENT TABLE (#PCDATA)>\n" +
         "	<!ELEMENT PROPERTY (#PCDATA)>\n" +
         "]>\n\n";
-
-
-    public static void main(String[] args) {
-        System.out.print(header);
-    }
 }
