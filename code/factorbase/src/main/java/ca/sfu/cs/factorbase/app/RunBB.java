27b0553af79857ef31445f2f18077f2f2ac5201a
rmar3a <rkm3@sfu.ca>
2019-02-24 04:55:29 -0800

    Additional cleanup for RunBB.java.
    - Removed the helper methods as they didn't seem necessary.  Grouping
      the lines that belong together with comments is clear enough.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
@@ -20,38 +20,24 @@ import ca.sfu.cs.factorbase.util.LoggerConfig;
  * @database@_setup (preconditions for learning)
  */
 public class RunBB {
-    static String isAutomaticSetup;
     private static Logger logger = Logger.getLogger(RunBB.class.getName());
 
 
     public static void main(String[] args) throws Exception {
-        LoggerConfig.setGlobalLevel();
         long t1 = System.currentTimeMillis();
+        LoggerConfig.setGlobalLevel();
+        Config config = new Config();
         logger.info("Start Program...");
-        setVarsFromConfig();
-        if (isAutomaticSetup.equals("1")) {
+
+        // Generate the setup database if specified to.
+        if (config.getProperty("AutomaticSetup").equals("1")) {
             MakeSetup.runMS();
             logger.info("Setup database is ready.");
         } else {
             logger.info("Setup database exists.");
         }
-        runBBLearner();
 
-        long t2 = System.currentTimeMillis();
-        logger.info("Total Running time is " + (t2 - t1) + "ms.");
-    }
-
-
-    public static void setVarsFromConfig() {
-        Config conf = new Config();
-        // 0: Do not run setup.
-        // 1: Run setup.
-        isAutomaticSetup = conf.getProperty("AutomaticSetup");
-    }
-
-
-    public static void runBBLearner() throws Exception {
-        // Assumes that "dbname" is in the config file and that dbname_setup exists.
+        // Learn a Bayesian Network.
         BayesBaseCT_SortMerge.buildCT();
         logger.info("The CT database is ready for use.");
         logger.info("*********************************************************");
@@ -61,9 +47,12 @@ public class RunBB {
         BayesBaseH.runBBH();
         logger.info("\nFinish running BayesBaseH.");
         logger.info("*********************************************************");
-        logger.info("Cleaning CT database");
 
         // Now eliminate temporary tables. Keep only the tables for the longest Rchain. Turn this off for debugging.
+        logger.info("Cleaning CT database");
         KeepTablesOnly.Drop_tmpTables();
+
+        long t2 = System.currentTimeMillis();
+        logger.info("Total Running time is " + (t2 - t1) + "ms.");
     }
 }
