5498c8c5a72189666c84a28eb43f3a1ce820b201
rmar3a <rkm3@sfu.ca>
2019-02-24 04:23:22 -0800

    Made white-space in RunBB.java consistent.
    - Replaced the mixture of tabs and spaces with spaces.
    - Made the white-space for the file consistent.
    
    Note: In general it's probably not a good idea to make white-space
          changes without making actual changes to the code as it makes
          the Git history more messy; however, there aren't too many
          commits for this file yet and most of the commits are just
          changes to the logging mechanism used by the program.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
@@ -10,60 +10,60 @@ import ca.sfu.cs.factorbase.tables.KeepTablesOnly;
 import ca.sfu.cs.factorbase.tables.MakeSetup;
 import ca.sfu.cs.factorbase.util.LoggerConfig;
 
-/* July 3rd, 2014, zqian
- * input: 
+/**
+ * July 3rd, 2014, zqian
+ * input:
  * @database@ (original data based on ER diagram)
- * output: 
+ * output:
  * @database@_BN (e.g. _CP, Path_BayesNets, Score)
- * 			@database@_CT (e.g. BiggestRchain_CT)
- * 			@database@_setup (preconditions for learning)
- * 
- * */
+ * @database@_CT (e.g. BiggestRchain_CT)
+ * @database@_setup (preconditions for learning)
+ */
 public class RunBB {
-	static String isAutomaticSetup;
-	private static Logger logger = Logger.getLogger(RunBB.class.getName());
-	
-	public static void main(String[] args) throws Exception {
-		LoggerConfig.setGlobalLevel();
-		long t1 = System.currentTimeMillis(); 
-		logger.info("Start Program...");
-		setVarsFromConfig();
-		if (isAutomaticSetup.equals("1")) {
-			MakeSetup.runMS();
-			logger.info("Setup database is ready.");
-		} else {
-			logger.info("Setup database exists.");
-		}
-		runBBLearner();
-		
-		long t2 = System.currentTimeMillis(); 
-		logger.info("Total Running time is " + (t2-t1) + "ms.");
-	}
-	
-	
-	public static void setVarsFromConfig(){
-		Config conf = new Config();
-		//1: run Setup; 0: not run
-		isAutomaticSetup = conf.getProperty("AutomaticSetup");
-	}
-	
-	public static void runBBLearner() throws Exception {
-		
-		//assumes that dbname is in config file and that dbname_setup exists.
+    static String isAutomaticSetup;
+    private static Logger logger = Logger.getLogger(RunBB.class.getName());
 
-		BayesBaseCT_SortMerge.buildCT();
-		logger.info("The CT database is ready for use.");
-		logger.info("*********************************************************");
-		CSVPrecomputor.runCSV();
-		logger.info("CSV files are generated.");
-		logger.info("*********************************************************");
-		BayesBaseH.runBBH();
-		logger.info("\nFinish running BayesBaseH.");
-		logger.info("*********************************************************");
-		logger.info("Cleaning CT database");
-		//Now eliminate temporary tables. Keep only the tables for the longest Rchain. Turn this off for debugging.//
-		KeepTablesOnly.Drop_tmpTables();
 
-	}
+    public static void main(String[] args) throws Exception {
+        LoggerConfig.setGlobalLevel();
+        long t1 = System.currentTimeMillis();
+        logger.info("Start Program...");
+        setVarsFromConfig();
+        if (isAutomaticSetup.equals("1")) {
+            MakeSetup.runMS();
+            logger.info("Setup database is ready.");
+        } else {
+            logger.info("Setup database exists.");
+        }
+        runBBLearner();
 
+        long t2 = System.currentTimeMillis();
+        logger.info("Total Running time is " + (t2 - t1) + "ms.");
+    }
+
+
+    public static void setVarsFromConfig() {
+        Config conf = new Config();
+        // 0: Do not run setup.
+        // 1: Run setup.
+        isAutomaticSetup = conf.getProperty("AutomaticSetup");
+    }
+
+
+    public static void runBBLearner() throws Exception {
+        // Assumes that "dbname" is in the config file and that dbname_setup exists.
+        BayesBaseCT_SortMerge.buildCT();
+        logger.info("The CT database is ready for use.");
+        logger.info("*********************************************************");
+        CSVPrecomputor.runCSV();
+        logger.info("CSV files are generated.");
+        logger.info("*********************************************************");
+        BayesBaseH.runBBH();
+        logger.info("\nFinish running BayesBaseH.");
+        logger.info("*********************************************************");
+        logger.info("Cleaning CT database");
+
+        // Now eliminate temporary tables. Keep only the tables for the longest Rchain. Turn this off for debugging.
+        KeepTablesOnly.Drop_tmpTables();
+    }
 }
