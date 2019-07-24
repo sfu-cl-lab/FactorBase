36d9910efa1f3cd2db4e6ec2fa1b16b9f8b07d2b
rmar3a <rkm3@sfu.ca>
2019-07-23 18:32:25 -0700

    Made white-space in BIF_Generator.java consistent.
    - Replaced the mixture tabs and spaces with spaces.
    - Made the white-space for method definitions and calls consistent.
    - Also made the white-space between assignments ("="), operators ("+"),
      and commas consistent.
    - Fixed up the comments and adjusted their formats.
    - Made the keywords for the SQL syntax in all CAPs to be consistent
      with other files.
    - Made a backup copy of CPGenerator.java in the obsolete directory
      so we can view the file prior to this change more easily if we need
      to.
    
    Note: In general it's probably not a good idea to make white-space
          changes without making actual changes to the code as it makes
          the Git history more messy; however, there aren't too many commits
          for this file yet so this should be okay.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/learning/CPGenerator.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/learning/CPGenerator.java
@@ -6,122 +6,110 @@ import com.mysql.jdbc.Connection;
 
 import java.io.IOException;
 import java.sql.*;
-/*@Jun 6, zqian
+
+/**
+ * @Jun 6, zqian
  * generating CP tables by calling a store procedure in _BN database
  * And also computing BIC,AIC,loglikelihood
- * 
- * 
- *August 9. This just adds Boolean values for the relationship values to the Attribute_Values tables. Todo: move this to Find_Values part.
- * 
- * */
+ *
+ * August 9. This just adds Boolean values for the relationship values to the Attribute_Values tables. Todo: move this to Find_Values part.
+ */
 import java.util.logging.Logger;
 
 public class CPGenerator {
 
-	static Connection  con2;
+    static Connection  con2;
 
-	static String databaseName, databaseName2 ,databaseName3;
-	static String dbUsername;
-	static String dbPassword;
-	static String dbaddress;
-	
-	private static Logger logger = Logger.getLogger(CPGenerator.class.getName());
-	
-	public static void main(String[] args) throws Exception {
-		
-		//read config file
-		setVarsFromConfig();
-		
-		//connect to db using jdbc
-		connectDB();
-        logger.info(" Parameter learning  for :"+ databaseName2);
+    static String databaseName, databaseName2 ,databaseName3;
+    static String dbUsername;
+    static String dbPassword;
+    static String dbaddress;
 
-		CPGenerator.Generator(databaseName,con2);
-		CP mycp = new CP(databaseName2,databaseName3);
-		mycp.cp();
-		logger.info("\n Parameter learning is done.");
-		
-		
-		con2.close();
-	}
+    private static Logger logger = Logger.getLogger(CPGenerator.class.getName());
 
-	
+    public static void main(String[] args) throws Exception {
+        //read config file
+        setVarsFromConfig();
+
+        // Connect to db using jdbc.
+        connectDB();
+        logger.info("Parameter learning for: " + databaseName2);
+
+        CPGenerator.Generator(databaseName,con2);
+        CP mycp = new CP(databaseName2,databaseName3);
+        mycp.cp();
+        logger.info("\n Parameter learning is done.");
+        con2.close();
+    }
 
 
-	public static void  Generator( String databaseName2,Connection con2) throws SQLException, IOException{
+    public static void  Generator(String databaseName2, Connection con2) throws SQLException, IOException {
 //        long l = System.currentTimeMillis();
-		Statement st1 = con2.createStatement();
-		// adding possible values of Rnodes into Attribute_Value //Jun 6
-		ResultSet rs1 = st1.executeQuery("select rnid from RNodes;;");    
-		while(rs1.next()){
-			String rnid = rs1.getString("rnid");
-			//logger.fine("rnid : " + rnid);
-			Statement st2 = con2.createStatement();
-			st2.execute("SET SQL_SAFE_UPDATES=0;");
-			//adding boolean values for rnodes
-			st2.execute("delete from  Attribute_Value  where column_name='"+rnid+"';");
-			//logger.fine("delete from  Attribute_Value  where column_name='"+rnid+"';");
-			//st2.execute("insert  into Attribute_Value values('"+rnid+"','True');");
-			//st2.execute("insert  into Attribute_Value values('"+rnid+"','False');");
-			st2.execute("insert  into Attribute_Value values('"+rnid+"','T');"); // April 28, 2014, zqian
-			st2.execute("insert  into Attribute_Value values('"+rnid+"','F');"); // keep consistency with ct table
-		}
-	// we used to make stored procedure. Now we do CP estimation in Java.
-		
-		/* st1.execute("drop procedure if exists `CP_Generator`;"); */
-		
-		
-	/*	//build stored procedure in _BN        
-        BZScriptRunner bzsr = new BZScriptRunner(databaseName2,con2);
+        Statement st1 = con2.createStatement();
+
+        // Adding possible values of Rnodes into Attribute_Value. // Jun 6.
+        ResultSet rs1 = st1.executeQuery("SELECT rnid FROM RNodes;");
+        while(rs1.next()) {
+            String rnid = rs1.getString("rnid");
+//            logger.fine("rnid : " + rnid);
+            Statement st2 = con2.createStatement();
+            st2.execute("SET SQL_SAFE_UPDATES = 0;");
+
+            // Adding boolean values for rnodes.
+            st2.execute("DELETE FROM  Attribute_Value WHERE column_name = '" + rnid + "';");
+//            logger.fine("DELETE FROM  Attribute_Value WHERE column_name = '" + rnid + "';");
+//            st2.execute("INSERT INTO Attribute_Value VALUES('" + rnid + "', 'True');");
+//            st2.execute("INSERT INTO Attribute_Value VALUES('" + rnid + "', 'False');");
+            st2.execute("INSERT INTO Attribute_Value VALUES('" + rnid + "', 'T');"); // April 28, 2014, zqian.
+            st2.execute("INSERT INTO Attribute_Value VALUES('" + rnid + "', 'F');"); // Keep consistency with CT table.
+        }
+
+        // We used to make stored procedure. Now we do CP estimation in Java.
+//        st1.execute("drop procedure if exists `CP_Generator`;");
+/*
+        // Build stored procedure in _BN.
+        BZScriptRunner bzsr = new BZScriptRunner(databaseName2, con2);
         bzsr.CP_createSP("scripts/CPGenerator.sql");
-        //logger.fine("creating the stored procedure is done "+databaseName);
+//        logger.fine("Creating the stored procedure is done " + databaseName);
         bzsr.callSP("CP_Generator");
-        //logger.fine("CP_Generator is done for "+databaseName);
+//        logger.fine("CP_Generator is done for " + databaseName);
         long l2 = System.currentTimeMillis();
-		System.out.print("Parameter Learning Time(ms): "+(l2-l)+" ms.\n");
+        System.out.print("Parameter Learning Time(ms): " + (l2 - l) + " ms.\n");
 
-        ResultSet rs2 = st1.executeQuery("SELECT    sum(LogLikelihood) as FinalLogLikelihood,    sum(Parameters) as FreeParameters,    sum(BIC) as FinalBIC,    sum(AIC) as FinalAIC FROM   Scores;");    
-		while(rs2.next()){
-			String FinalLogLikelihood = rs2.getString("FinalLogLikelihood");
-			logger.fine("FinalLogLikelihood : " + FinalLogLikelihood);
-			String FreeParameters = rs2.getString("FreeParameters");
-			logger.fine("FreeParameters : " + FreeParameters);
-			String  FinalBIC = rs2.getString("FinalBIC");
-			logger.fine("FinalBIC  : " + FinalBIC);
-			String  FinalAIC = rs2.getString("FinalAIC");
-			logger.fine("FinalAIC  : " + FinalAIC);
-			
-		}
-		*/
-        
-        
-        
-	}
-	
-	public static void setVarsFromConfig(){
-		Config conf = new Config();
-		databaseName = conf.getProperty("dbname");
-		databaseName2 = databaseName + "_BN";
-		databaseName3 = databaseName + "_CT";
-
-		dbUsername = conf.getProperty("dbusername");
-		dbPassword = conf.getProperty("dbpassword");
-		dbaddress = conf.getProperty("dbaddress");
-	}
-
-	public static void connectDB() throws SQLException {
-
-		String CONN_STR2 = "jdbc:" + dbaddress + "/" + databaseName2;
-		try {
-			java.lang.Class.forName("com.mysql.jdbc.Driver");
-		} catch (Exception ex) {
-			logger.severe("Unable to load MySQL JDBC driver");
-		}
-		con2 = (Connection) DriverManager.getConnection(CONN_STR2, dbUsername, dbPassword);
-		
-		
-	}
+        ResultSet rs2 = st1.executeQuery("SELECT SUM(LogLikelihood) AS FinalLogLikelihood, SUM(Parameters) AS FreeParameters, SUM(BIC) AS FinalBIC, SUM(AIC) AS FinalAIC FROM Scores;");
+        while(rs2.next()) {
+            String FinalLogLikelihood = rs2.getString("FinalLogLikelihood");
+            logger.fine("FinalLogLikelihood: " + FinalLogLikelihood);
+            String FreeParameters = rs2.getString("FreeParameters");
+            logger.fine("FreeParameters: " + FreeParameters);
+            String FinalBIC = rs2.getString("FinalBIC");
+            logger.fine("FinalBIC: " + FinalBIC);
+            String FinalAIC = rs2.getString("FinalAIC");
+            logger.fine("FinalAIC: " + FinalAIC);
+         }
+*/
+    }
 
 
-	
+    public static void setVarsFromConfig() {
+        Config conf = new Config();
+        databaseName = conf.getProperty("dbname");
+        databaseName2 = databaseName + "_BN";
+        databaseName3 = databaseName + "_CT";
+        dbUsername = conf.getProperty("dbusername");
+        dbPassword = conf.getProperty("dbpassword");
+        dbaddress = conf.getProperty("dbaddress");
+    }
+
+
+    public static void connectDB() throws SQLException {
+
+        String CONN_STR2 = "jdbc:" + dbaddress + "/" + databaseName2;
+        try {
+            java.lang.Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception ex) {
+            logger.severe("Unable to load MySQL JDBC driver");
+        }
+        con2 = (Connection) DriverManager.getConnection(CONN_STR2, dbUsername, dbPassword);
+    }
 }
