8fc466deeea0fb922992a204763efd182c170861
rmar3a <rkm3@sfu.ca>
2019-07-31 14:44:45 -0700

    Made white-space in LoggerConfig.java consistent.
    - Replaced the mixture tabs and spaces with spaces.
    - Fixed up the comments and adjusted their formats.
    
    Note: In general it's probably not a good idea to make white-space
          changes without making actual changes to the code as it makes
          the Git history more messy; however, there aren't too many commits
          for this file yet so this should be okay.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/util/LoggerConfig.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/util/LoggerConfig.java
@@ -9,50 +9,52 @@ import java.util.logging.Level;
 
 import ca.sfu.cs.common.Configuration.Config;
 
-public class LoggerConfig {	
-	/**
-	 * Set the global logger level
-	 * debug: show all log messages(including debug, info, warning and error messages)
-	 * info: only show info, warning and error messages(no debug message)
-	 * off: show no log message
-	 * @throws Exception if the specified value is not valid
-	 */
-	public static Map<String, Level> levelMap;
-	
-	public static void setGlobalLevel() throws Exception{
-		buildLevelMap();
-		Level loggerLevel = getLevelFromConfig();
-		Logger rootLogger = LogManager.getLogManager().getLogger("");
-		
-		rootLogger.setLevel(loggerLevel);
-		for (Handler h : rootLogger.getHandlers()) {
-		    h.setLevel(loggerLevel);
-		}
-	}
-	
-	/**
-	 *Build the Level table to map the LoggingLevel specified in the config file to the Level of JUL
-	 */
-	public static void buildLevelMap() {
-		levelMap = new HashMap<String, Level>();
-		levelMap.put("debug", Level.ALL);
-		levelMap.put("info", Level.INFO);
-		levelMap.put("off", Level.OFF);	
-	}
-	
-	/**
-	 * get the logger level from config.cfg
-	 * @return the specified Level
-	 * @throws Exception if the specified value is not valid
-	 */
-	private static Level getLevelFromConfig() throws Exception{
-		String loggingLevel;
-		Config conf = new Config();
-		loggingLevel = conf.getProperty("LoggingLevel");
-				
-		if(levelMap.get(loggingLevel) == null) {
-			 throw new IllegalArgumentException("Invlid LoggingLevel setting. Please set the loggingLevel in config.cfg to debug/info/off!");
-		}
-		return levelMap.get(loggingLevel);
-	}
+public class LoggerConfig {
+    /**
+     * Set the global logger level.
+     * debug: show all log messages (including debug, info, warning and error messages).
+     * info: only show info, warning and error messages (no debug message).
+     * off: show no log message.
+     *
+     * @throws Exception if the specified value is not valid
+     */
+    public static Map<String, Level> levelMap;
+
+    public static void setGlobalLevel() throws Exception{
+        buildLevelMap();
+        Level loggerLevel = getLevelFromConfig();
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(loggerLevel);
+        for (Handler h : rootLogger.getHandlers()) {
+            h.setLevel(loggerLevel);
+        }
+    }
+
+    /**
+     * Build the Level table to map the LoggingLevel specified in the config file to the Level of JUL.
+     */
+    public static void buildLevelMap() {
+        levelMap = new HashMap<String, Level>();
+        levelMap.put("debug", Level.ALL);
+        levelMap.put("info", Level.INFO);
+        levelMap.put("off", Level.OFF);
+    }
+
+    /**
+     * Get the logger level from config.cfg.
+     *
+     * @return the specified Level.
+     * @throws Exception if the specified value is not valid.
+     */
+    private static Level getLevelFromConfig() throws Exception {
+        String loggingLevel;
+        Config conf = new Config();
+        loggingLevel = conf.getProperty("LoggingLevel");
+
+        if (levelMap.get(loggingLevel) == null) {
+            throw new IllegalArgumentException("Invlid LoggingLevel setting. Please set the loggingLevel in config.cfg to debug/info/off!");
+        }
+
+        return levelMap.get(loggingLevel);
+    }
 }
