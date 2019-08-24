e3bc94e8218e4483eab66faa791a0d8b6a72d3dc
rmar3a <rkm3@sfu.ca>
2019-08-23 20:03:31 -0700

    [performance] (5/7) ContingencyTable uses column indices.
    - Updated ContingencyTable to use column indices rather than column
      names since column indices are now passed to
      ContingencyTableGenerator instead of column names.
    - Updated the associated tests.
    - Added more comments to the code.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/data/ContingencyTable.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/data/ContingencyTable.java
@@ -1,44 +1,83 @@
 package ca.sfu.cs.factorbase.data;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * The information available from a given contingency (CT) table.
  */
 public class ContingencyTable {
-
     private long[] countsArray;
-    private List<String> variables;
+    private int childColumnIndex;
+    private int[] parentColumnIndices;
     private DataSetMetaData metadata;
 
 
     /**
      * Create a Java representation of a CT table, which consists of counts and metadata.
-     * @param countsArray - array containing counts for the groundings of all the variables in the
-     *                      given List {@code variables}.
-     * @param variables - List containing all the variables for the CT table.
+     *
+     * @param countsArray - array containing counts for the groundings of the given child and parent variables.
+     * @param childColumnIndex - the column index (from the original dataset) of the child variable.
+     * @param parentColumnIndices - the column indices (from the original dataset) of the parent variables.
      * @param metadata - metadata from the dataset used to generate the given Array {@code countsArray}.
      */
-    public ContingencyTable(long[] countsArray, List<String> variables, DataSetMetaData metadata) {
+    public ContingencyTable(
+        long[] countsArray,
+        int childColumnIndex,
+        int[] parentColumnIndices,
+        DataSetMetaData metadata
+    ) {
         this.countsArray = countsArray;
-        this.variables = variables;
+        this.childColumnIndex = childColumnIndex;
+        this.parentColumnIndices = parentColumnIndices;
         this.metadata = metadata;
     }
 
 
     /**
      * Retrieve the number of times a particular instance (grounding) occurs.
+     * <p>
+     * <b>IMPORTANT</b>: The list of parent {@code RandomVariableAssignment}s must be in the same order as the parent
+     *                   column indices passed to {@link ContingencyTableGenerator#generateCT(int, int[], int)}.
+     * </p>
      *
-     * @param randomVariableAssignments - list of attribute assignments for the grounding of interest.
+     * @param childAssignment - the child random variable to get the counts for.
+     * @param parentAssignments - the parent random variables to get the counts for.
      * @return the number of times the grounding occurs.
      */
-    public long getCounts(List<RandomVariableAssignment> randomVariableAssignments) {
-        if (!this.variables.equals(randomVariableAssignments.stream().map(assignment -> assignment.getName()).collect(Collectors.toList()))) {
+    public long getCounts(RandomVariableAssignment childAssignment, List<RandomVariableAssignment> parentAssignments) {
+        if (this.childColumnIndex != childAssignment.getVariableColumnIndex()) {
+            throw new IllegalArgumentException(
+                "Counts can only be retrieved from a contingency table when the full grounding is given with the " +
+                "child variable given first followed by its parents."
+            );
+        }
+
+        int totalNumberOfVariables = parentAssignments.size() + 1;
+        RandomVariableAssignment[] selectedRandomVariables = new RandomVariableAssignment[totalNumberOfVariables];
+        selectedRandomVariables[0] = childAssignment;
+
+        /**
+         * Validate if the full grounding is given for the parents and that they are in the same order as when the CT
+         * table was generated.  Combine the parents with the child so that they can be used together to generate the
+         * grounding key.
+         */
+        if (!parentAssignments.isEmpty()) {
+            int insertIndex = 1;
+            int index = 0;
+            for (RandomVariableAssignment parentAssignment : parentAssignments) {
+                if (this.parentColumnIndices[index] != parentAssignment.getVariableColumnIndex()) {
+                    throw new IllegalArgumentException("Counts can only be retrieved from a contingency table when the full grounding is given.");
+                }
+
+                selectedRandomVariables[insertIndex] = parentAssignment;
+                insertIndex++;
+                index++;
+            }
+        } else if(this.parentColumnIndices.length != 0) {
             throw new IllegalArgumentException("Counts can only be retrieved from a contingency table when the full grounding is given.");
         }
 
-        int stateIndex = this.metadata.generateIndex(randomVariableAssignments);
-        return this.countsArray[stateIndex];
+        int countsIndex = this.metadata.generateIndex(selectedRandomVariables);
+        return this.countsArray[countsIndex];
     }
 }
