51ac21d0b2b7c2419f3e8b67d590737dd336508c
rmar3a <rkm3@sfu.ca>
2019-09-10 16:46:47 -0700

    Cleaned up Config.java.
    - Removed unnecessary code in Config.java, e.g. removing the unused
      method closeFile().

--- code/common/src/main/java/ca/sfu/cs/common/Configuration/Config.java
+++ code/common/src/main/java/ca/sfu/cs/common/Configuration/Config.java
@@ -1,22 +1,14 @@
 package ca.sfu.cs.common.Configuration;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 public class Config {
     public static final String SCRIPTS_DIRECTORY = "scripts/";
     public static final String DEFAULT_CONFIGFILE = "config.cfg";
 
     Properties configFile;
-    FileReader fr;
-    Reader reader;
     
-    private static Logger logger = Logger.getLogger(Config.class.getName());
-
     public Config() {
         configFile = new java.util.Properties();
 
@@ -25,10 +17,8 @@ public class Config {
             config = DEFAULT_CONFIGFILE;
         }
 
-        try {
-            fr = new FileReader(config);
-            reader = new BufferedReader(fr);
-            configFile.load(reader);
+        try (FileReader fileReader = new FileReader(config)) {
+            configFile.load(fileReader);
         } catch(Exception eta) {
             eta.printStackTrace();
         }
@@ -36,18 +26,5 @@ public class Config {
 
     public String getProperty(String key) {
         return this.configFile.getProperty(key);
-    }
-
-    public int closeFile() {
-        try {
-            reader.close();
-            fr.close();
-        } catch (IOException e) {
-            logger.severe("Failed to close file.");
-            e.printStackTrace();
-            return -1;
-        }
-
-        return 0;
     }
 }
