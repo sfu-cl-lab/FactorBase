c35a148aed5240b8ac7c50aba9a22ad2b2c2f4cc
rmar3a <rkm3@sfu.ca>
2019-12-03 12:18:36 -0800

    Moved the logRunTime() helper method into a separate class.
    - Moved the logRunTime() helper method in RunBB.java into a separate
      class and updated any references to this method.

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/app/RunBB.java
@@ -15,6 +15,8 @@ import ca.sfu.cs.factorbase.lattice.RelationshipLattice;
 import ca.sfu.cs.factorbase.learning.BayesBaseH;
 import ca.sfu.cs.factorbase.learning.CountsManager;
 import ca.sfu.cs.factorbase.util.LoggerConfig;
+import ca.sfu.cs.factorbase.util.RuntimeLogger;
+
 import nu.xom.ParsingException;
 
 /**
@@ -39,7 +41,7 @@ public class RunBB {
         Config config = new Config();
         long configEnd = System.currentTimeMillis();
         logger.info("Start Program...");
-        logRunTime(logger, "Logger + Config Initialization", start, configEnd);
+        RuntimeLogger.logRunTime(logger, "Logger + Config Initialization", start, configEnd);
 
         long databaseStart = System.currentTimeMillis();
         FactorBaseDataBase factorBaseDatabase = new MySQLFactorBaseDataBase(
@@ -49,7 +51,7 @@ public class RunBB {
             config.getProperty("dbusername"),
             config.getProperty("dbpassword")
         );
-        logRunTime(logger, "Creating Database Connection", databaseStart, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Creating Database Connection", databaseStart, System.currentTimeMillis());
 
         // Generate the initial databases if specified to.
         long setupStart = System.currentTimeMillis();
@@ -58,23 +60,23 @@ public class RunBB {
         } else {
             logger.info("Databases are assumed to be setup!");
         }
-        logRunTime(logger, "Creating Setup Database", setupStart, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Creating Setup Database", setupStart, System.currentTimeMillis());
 
         // Generate the relationship lattice to guide the structure learning search.
         long globalLatticeStart = System.currentTimeMillis();
         RelationshipLattice globalLattice = factorBaseDatabase.getGlobalLattice();
-        logRunTime(logger, "Creating Global Lattice", globalLatticeStart, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Creating Global Lattice", globalLatticeStart, System.currentTimeMillis());
 
         // Learn a Bayesian Network.
         boolean usePreCounting = config.getProperty("PreCounting").equals("1");
         if (usePreCounting) {
             long buildCTStart = System.currentTimeMillis();
             CountsManager.buildCT();
-            logRunTime(logger, "Creating CT Tables", buildCTStart, System.currentTimeMillis());
+            RuntimeLogger.logRunTime(logger, "Creating CT Tables", buildCTStart, System.currentTimeMillis());
         } else {
             long buildGlobalCountsStart = System.currentTimeMillis();
             CountsManager.buildRChainsGlobalCounts();
-            logRunTime(logger, "Creating Global Counts Tables", buildGlobalCountsStart, System.currentTimeMillis());
+            RuntimeLogger.logRunTime(logger, "Creating Global Counts Tables", buildGlobalCountsStart, System.currentTimeMillis());
         }
 
         long bayesBaseHStart = System.currentTimeMillis();
@@ -83,27 +85,15 @@ public class RunBB {
             globalLattice,
             usePreCounting
         );
-        logRunTime(logger, "Running BayesBaseH", bayesBaseHStart, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Running BayesBaseH", bayesBaseHStart, System.currentTimeMillis());
 
         // Now eliminate temporary tables. Keep only the tables for the longest Rchain. Turn this off for debugging.
         long cleanupStart = System.currentTimeMillis();
         factorBaseDatabase.cleanupDatabase();
-        logRunTime(logger, "Cleanup Database", cleanupStart, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Cleanup Database", cleanupStart, System.currentTimeMillis());
 
-        logRunTime(logger, "Total", start, System.currentTimeMillis());
+        RuntimeLogger.logRunTime(logger, "Total", start, System.currentTimeMillis());
 
         logger.info("Program Done!");
-    }
-
-
-    /**
-     * Helper method to write out the run times in a consistent format.
-     * @param logger - the logger to write the runtime to.
-     * @param stage - the part of the FactorBase program that was run.
-     * @param start - the start time for the given stage (ms).
-     * @param end - the end time for the given stage (ms).
-     */
-    private static void logRunTime(Logger logger, String stage, long start, long end) {
-        logger.info("Runtime[" + stage + "]: " + String.valueOf(end - start) + "ms.");
     }
 }
---
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/util/RuntimeLogger.java
@@ -0,0 +1,20 @@
+package ca.sfu.cs.factorbase.util;
+
+import java.util.logging.Logger;
+
+/**
+ * Class to help log information for a FactorBase run.
+ */
+public class RuntimeLogger {
+
+    /**
+     * Helper method to write out the run times in a consistent format.
+     * @param logger - the logger to write the runtime to.
+     * @param stage - the part of the FactorBase program that was run.
+     * @param start - the start time for the given stage (ms).
+     * @param end - the end time for the given stage (ms).
+     */
+    public static void logRunTime(Logger logger, String stage, long start, long end) {
+        logger.info("Runtime[" + stage + "]: " + String.valueOf(end - start) + "ms.");
+    }
+}
