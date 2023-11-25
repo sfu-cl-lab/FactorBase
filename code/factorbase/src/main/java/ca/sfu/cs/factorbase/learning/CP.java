5a4652975a4c076a883b7dea419ff1ae2baa8e87
Mingyi Wu <71236927+greedcat@users.noreply.github.com>
2023-11-25 02:47:56 -0800

    Update CP.java

--- code/factorbase/src/main/java/ca/sfu/cs/factorbase/learning/CP.java
+++ code/factorbase/src/main/java/ca/sfu/cs/factorbase/learning/CP.java
@@ -317,7 +317,7 @@ public class CP {
         st.execute(
             "CREATE TABLE `" + tableName + "` (" +
                 "`" + nodeName + "` VARCHAR(200) NOT NULL, " +
-                "CP FLOAT(10,10), " +
+                "CP FLOAT(11,10), " +
                 "MULT DECIMAL(65), " +
                 "local_mult DECIMAL(65)" +
             ");"
